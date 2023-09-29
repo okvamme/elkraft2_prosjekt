a0eccb01c7b2070fe1603bfb55498c89fb39236e
marieler <145976684+marieler@users.noreply.github.com>
2023-09-29 12:23:04 +0200

    Update PandQcalculated.py

--- PandQcalculated.py
+++ PandQcalculated.py
@@ -1,5 +1,4 @@
 import math
-from classes import list_buses
 
 def P_calculated(bus, list_buses, Y_bus):
     #bus is the specific bus you are calculating P for
@@ -16,7 +15,7 @@ def Q_calculated(bus, list_buses, Y_bus):
     #bus is the specific bus you are calculating Q for
     #list_buses is a list of all instances of the class bus
     #Y_bus is the Ybus for the system
-    
+
     Q_calc = 0
     for b in list_buses:
         Q_calc += bus.Va * b.Va * (Y_bus[bus.a - 1 , b.a - 1].real * math.sin(bus.da - b.da) - Y_bus[bus.a - 1 , b.a - 1].imag *math.cos(bus.da - b.da))
